9c14e7b99d15a419d8d85963d8d45b7f0c16d95b
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-11 23:51:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {surfaces: 'bloom'};
 
 
-var updatePlaylist = {
+var updatePlaylist(playlist,'Phil Ochs', "Here's to the State of Mississippi") = {
 Slowdive : 'Alison',
 MyBloodyValentine : 'Sometimes',
 PhilOchs : "Here\'s to the State of Mississippi"
