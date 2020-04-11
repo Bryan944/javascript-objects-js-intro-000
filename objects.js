a1a274081afc6028971a3d58931c0bb25f484736
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-11 23:34:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {surfaces: 'bloom'};
 playlist.Slowdive = 'Alison';
 playlist.MyBloodyValentine = 'Sometimes';
 playlist.PhilOchs = "Here\'s to the State of Mississippi";
-
+console.log(playlist)
 delete playlist.Slowdive;
 console.log(playlist);
