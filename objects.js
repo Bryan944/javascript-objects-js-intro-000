e587b6123bca7cd0b4abdd8014ff583b48f43ade
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-12 02:30:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,8 @@ var playlist = {surfaces: 'bloom'};
 
 //var playlist = {surfaces: 'bloom'};
 function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Slowdive'] = 'Alison'
+  playlist['My Bloody Valentine'] = 'Sometimes'
   playlist[artistName] = songTitle;
   return playlist;
 }
