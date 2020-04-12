f41b5e6904f2a63ff80e5f6673027368aff7fcf1
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-12 00:37:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,8 @@ function updatePlaylist(playlist,artistName,songTitle){
   //var playlist = {surfaces: 'bloom'};
       playlist['Slowdive'] = 'Alison',
       playlist['My Bloody Valentine'] = 'Sometimes',
-
+playlist2 = playlist;
   playlist = Object.assign({}, playlist, { [artistName]: songTitle })
-  return playlist;
+  
+  return playlist2;
 }
