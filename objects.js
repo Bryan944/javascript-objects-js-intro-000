dde7701fc3da683719abc168a81a0cc50faacf4d
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-12 02:36:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
 }
+
+
+function removeFromPlaylist(playlist,artistName){
+      delete playlist.Slowdive;
+  return playlist;
+}
